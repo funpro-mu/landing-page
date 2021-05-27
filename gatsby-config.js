cfe56a4bad5def3e7d44f1193a3412ea91182af4
Clarice Bouwer <cbillowes@gmail.com>
2021-05-27 19:55:46 +0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  siteMetadata: {
+    title: "funpro.mu-landing",
+  },
+  plugins: [
+    "gatsby-plugin-styled-components",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "273805619",
+      },
+    },
+  ],
+};

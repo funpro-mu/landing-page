a36a5d4b370783f1571df630190bda2df4011269
Clarice Bouwer <cbillowes@gmail.com>
2021-05-28 12:17:22 +0400

    Create landing page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,44 @@
 module.exports = {
   siteMetadata: {
-    title: "funpro.mu-landing",
+    title: "Mauritius Functional Programmers Community",
   },
   plugins: [
+    "gatsby-plugin-image",
+    `gatsby-plugin-sharp`,
     "gatsby-plugin-styled-components",
     {
       resolve: "gatsby-plugin-google-analytics",
       options: {
         trackingId: "273805619",
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `./src/images`,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /src\/images/
+        }
+      }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Mauritius Functional Programmers Community",
+        short_name: "funprog.mu",
+        start_url: "/",
+        background_color: "#212a39",
+        theme_color: "#ffc000",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "browser",
+        icon: "src/images/favicon.png",
       },
     },
   ],
